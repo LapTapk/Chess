e0ef11c4ae0a2408bcb3686f988c4202ae67e350
Alexey Anikiev <aaanikiev@edu.hse.ru>
2023-11-16 12:11:12 +0300

    Merged GameObject and Scene

--- src/engine/game_object.py
+++ src/engine/game_object.py
@@ -1,3 +1,4 @@
+from renderer import Renderer
 from vector2 import Vector2
 
 
@@ -38,3 +39,37 @@ class GameObject:
                 return comp
 
         return None
+
+
+class Scene:
+    '''
+    Scene stores objects that are currently present
+    and activates their functionality.
+
+    :param list[GameObject] objects: sets *objects* field
+    '''
+
+    def __init__(self, objects):
+        self.objects = objects
+        '''List of objects that are present in scene'''
+
+    def update(self):
+        '''
+        Method that invokes *update* of every game object's component
+        '''
+        for obj in self.objects:
+            for comp in obj.components:
+                comp.update()
+
+    def at_point(self, point):
+        '''
+        :rtype: list[Renderer]
+        :return: All objects that appear under the mouse
+        '''
+        res = []
+        for obj in self.objects:
+            rect = obj.get_component(Renderer).get_rect()
+            if rect.collidepoint(point):
+                res.append(obj)
+
+        return res
