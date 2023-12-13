846eb925533d3eeb60ae5ff66e5706db1b04d333
LapTapk <fret4657@gmail.com>
2023-12-14 00:26:08 +0300

    Fixed test_game_object and utils

--- tests/test_game_object.py
+++ tests/test_game_object.py
@@ -4,33 +4,32 @@ from src.engine.input_box import InputBox
 import utils
 
 
-class TestGameObject:
-    utils.game_pseudoinit()
+utils.game_pseudoinit()
 
-    def test_get_component_valid(self):
-        go = GameObject()
-        rend = Renderer()
-        ib = InputBox()
+def test_get_component_valid():
+    go = GameObject()
+    rend = Renderer()
+    ib = InputBox()
 
-        ib.init(go, None, None)
-        rend.init(go, None)
-        go.init(None, components=[ib, rend])
-        assert go.get_component(Renderer) == rend
+    ib.init(go, None, None)
+    rend.init(go, None)
+    go.init(None, components=[ib, rend])
+    assert go.get_component(Renderer) == rend
 
-    def test_get_component_nonvalid(self):
-        go = GameObject()
-        ib = InputBox()
+def test_get_component_nonvalid():
+    go = GameObject()
+    ib = InputBox()
 
-        ib.init(go, None, None)
-        go.init(None, components=[ib])
-        assert go.get_component(Renderer) == None
+    ib.init(go, None, None)
+    go.init(None, components=[ib])
+    assert go.get_component(Renderer) == None
 
-    def test_add_component(self):
-        go = GameObject()
-        ib = InputBox()
+def test_add_component():
+    go = GameObject()
+    ib = InputBox()
 
-        go.init(None)
-        ib.init(go, None, None)
+    go.init(None)
+    ib.init(go, None, None)
 
-        go.add_component(ib)
-        assert go.get_component(InputBox) == ib
+    go.add_component(ib)
+    assert go.get_component(InputBox) == ib
--- tests/utils.py
+++ tests/utils.py
@@ -3,5 +3,8 @@ from src.engine import game
 
 
 def game_pseudoinit(scr_size=(1280, 720), fps=60):
+    if game.is_init:
+        return
+
     pygame.display.set_mode = lambda x: pygame.surface.Surface(x)
     game.init(scr_size, fps, 'game_data.json')
