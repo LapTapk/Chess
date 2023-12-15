327c4bdea7762cebf87cca53ff96f35a5ad1e48c
LapTapk <fret4657@gmail.com>
2023-12-15 20:41:55 +0300

    Added docs dir and game_object docs

--- src/engine/game_object.py
+++ src/engine/game_object.py
@@ -1,42 +1,30 @@
 from . import game
 from .renderer import Renderer
 from .vector2 import Vector2
-
-
-class GameObject:
-    def init(self, scene, pos=Vector2(0, 0), rot=0, scale=Vector2(1, 1), components=[], children=[]):
-        self.rotation = rot
-
-        factor_x = game.screen_size[0] / \
-            game.data['default-screen-size'][0]
-        factor_y = game.screen_size[1] / \
-            game.data['default-screen-size'][1]
-        self.scale = Vector2(scale.x * factor_x, scale.y * factor_y)
-        self.position = Vector2(pos.x * factor_x, pos.y * factor_y)
-
-        self.components = components
-        self.scene = scene
-        self.children = children
-        self.enabled = True
-
-    def get_component(self, type):
-        for comp in self.components:
-            if isinstance(comp, type):
-                return comp
-
-
-    def add_component(self, comp):
-        self.components.append(comp)
-        comp.go = self
+from typing import *
 
 
 class Scene:
-    def init(self, *objects):
-        self.objects = list(objects)
+    '''
+    Сущность, являющаяся совокупностью всех элементарных частиц ``GameObject``.
+    '''
 
-    def __update_obj(self, obj):
+    def init(self, *objects):
+        '''
+        Инициализатор. Аналогичен __init__. 
+        Все параметры метода соответствуют полям класса.
+        '''
+        self.objects: List[GameObject] = list(objects)
+        '''Список объектов, принадлежащих текущей ``Scene``'''
+
+    def __update_obj(self, obj) -> None:
+        '''
+        Служебный метод, выполняющий ту же функцию, что и *update*.
+
+        :param obj: ``GameObject``, у которого нужно вызвать update.
+        '''
         if not obj.enabled:
-            return 
+            return
 
         for comp in obj.components:
             comp.update()
@@ -44,11 +32,21 @@ class Scene:
         for c_obj in obj.children:
             self.__update_obj(c_obj)
 
-    def update(self):
+    def update(self) -> None:
+        '''
+        Метод, вызывающий метод update 
+        для каждого компонента каждого объекта данной ``Scene``.
+        '''
         for obj in self.objects:
             self.__update_obj(obj)
 
-    def __at_point_rec(self, obj, point):
+    def __at_point_rec(self, obj, point: Tuple[int, int]) -> List[Any]:
+        '''
+        Служебный метод, выполняющий ту же функцию, что и *at_point*.
+
+        :param obj: ``GameObject``, для которого необходимо выполнить проверку на пересечение с точкой *point*.
+        :param point: точка, в которой осуществляется поиск ``GameObject``.
+        '''
         res = []
 
         rend = obj.get_component(Renderer)
@@ -62,7 +60,11 @@ class Scene:
 
         return res
 
-    def at_point(self, point):
+    def at_point(self, point: Tuple[int, int]) -> List[Any]:
+        '''
+        Метод, находящий все объекты, которые пересекают данную точку
+        рисунком ``Renderer``.
+        '''
         res = []
         for obj in self.objects:
             res.extend(self.__at_point_rec(obj, point))
@@ -70,5 +72,60 @@ class Scene:
         return res
 
     def add_object(self, go):
+        '''
+        Метод, добавляющий ``GameObject`` в ``Scene``.
+        '''
         self.objects.append(go)
         go.scene = self
+
+
+class GameObject:
+    '''
+    Элементарная сущность, хранящая характеристики и поведение,
+    согласно которому она должна действовать на сцене.
+    '''
+
+    def init(self, scene: Scene, pos=Vector2(0, 0), rot=0, scale=Vector2(1, 1), components=[], children=[]) -> None:
+        '''
+        Инициализатор. Аналогична __init__. Каждый параметр функции
+        соотносится с параметром класса.
+        '''
+        self.rotation: int = rot
+        '''Угол поворота ``GameObject`` в градусах.'''
+
+        factor_x = game.screen_size[0] / \
+            game.data['default-screen-size'][0]
+        factor_y = game.screen_size[1] / \
+            game.data['default-screen-size'][1]
+        self.scale: Vector2 = Vector2(scale.x * factor_x, scale.y * factor_y)
+        '''Коэффициент при размере ``GameObject``.'''
+        self.position: Vector2 = Vector2(pos.x * factor_x, pos.y * factor_y)
+        '''Расположение ``GameObject`` на сцене.'''
+
+        self.components: List[Any] = components
+        '''Компоненты, определяющие поведение ``GameObject``.'''
+        self.scene: Scene = scene
+        '''Cцена, в которой располагается этот ``GameObject``.'''
+        self.children: List[GameObject] = children
+        '''Дочерние ``GameObject``.'''
+        self.enabled: bool = True
+        '''Принимает ли участие данный ``GameObject`` в сцене'''
+
+    def get_component(self, type: Any) -> type:
+        '''
+        Метод, возвращающий компонент определенного типа 
+        из компонентов данного ``GameObject``.
+
+        :param type: тип искомого компонента.
+        :return: компонент данного типа.
+        '''
+        for comp in self.components:
+            if isinstance(comp, type):
+                return comp
+
+    def add_component(self, comp: Any) -> None:
+        '''
+        Процедура, добавляющая компонент в список компонентов ``GameObject``.
+        '''
+        self.components.append(comp)
+        comp.go = self
