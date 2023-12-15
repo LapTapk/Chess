8edc733b554ace8b6ae104af98457b6c617f7bec
LapTapk <fret4657@gmail.com>
2023-12-15 21:36:59 +0300

    Added game and chess_state_manager docs

--- src/engine/chess_state_machine.py
+++ src/engine/chess_state_machine.py
@@ -1,35 +1,72 @@
 import pygame
-from . import game
+import typing
+from . import game, game_object, grab, board
 
 
 class ChessStateMachine:
-    def init(self, go, grabber, brd_updater):
-        self.go = go
-        self.cur_state = None
-        self.user_turn_state = UserTurnState(self, grabber, brd_updater)
-        self.enemy_turn_state = EnemyTurnState(self, brd_updater)
+    '''
+    Компонент, определяющий поведение в зависимости от текущего хода.
+    '''
+
+    def init(self, go: game_object.GameObject, grabber: grab.Grabber, brd_updater: board.BoardUpdater) -> None:
+        '''Инициализатор. Аналогичен __init__. Все параметры соответствуют полям класса.'''
+        self.go: game_object.GameObject = go
+        '''``GameObject``, которому принадлежит текущий компонент.'''
+        self.cur_state: [UserTurnState | EnemyTurnState] = None
+        '''Текущее состояние сущности.'''
+        self.user_turn_state: UserTurnState = UserTurnState(
+            self, grabber, brd_updater)
+        '''Состояние хода пользователя.'''
+        self.enemy_turn_state: EnemyTurnState = EnemyTurnState(
+            self, brd_updater)
+        '''Состояние хода противника'''
         first_state = self.user_turn_state if game.clnt.is_white else self.enemy_turn_state
         self.change_state(first_state)
 
-    def change_state(self, new_state):
+    def change_state(self, new_state) -> None:
+        '''
+        Процедура смены состояния.
+
+        :param new_state: новое состояние.
+        :type new_state: [UserTurnState | EnemyTurnState]
+        '''
         self.cur_state = new_state
         self.cur_state.on_start()
 
-    def update(self):
+    def update(self) -> None:
+        '''Процедура обновления компонента для каждого кадра.'''
         self.cur_state.update()
 
 
 class UserTurnState:
-    def __init__(self, machine, grabber, brd_updater):
-        self.machine = machine
-        self.grabber = grabber
-        self.brd_updater = brd_updater
+    '''
+    Сущность состояния во время хода игрока. 
+    Обрабатывает поднятие фигуры, обновление доски и передачу хода противнику.
+    '''
 
-    def on_start(self):
+    def __init__(self, machine: ChessStateMachine, grabber: grab.Grabber, brd_updater: board.BoardUpdater) -> None:
+        self.machine = machine
+        '''``ChessStateMachine`` в котором вызывается данное состояние.'''
+        self.grabber = grabber
+        '''``Grabber``, ответственный за поднятие фигур с доски.'''
+        self.brd_updater = brd_updater
+        '''Средство обновления выводимой доски.'''
+
+    def on_start(self) -> None:
+        '''
+        Метод, вызываемый при обновлении состояния машиной.
+        Для ``UserTurnState`` обновляет доску.
+        '''
         brd = game.clnt.get_board()
         self.brd_updater.update_board(brd, game.clnt.is_white, True)
 
-    def handle_input(self):
+    def handle_input(self) -> None:
+        '''
+        Метод, обрабатывающий входные *event*.
+        Если ни одна фигура не поднята, то пытается поднять.
+        Если фигура ставится на стол, спрашивает у сервера легальность 
+        хода.
+        '''
         for event in game.events:
             if event.type != pygame.MOUSEBUTTONDOWN:
                 continue
@@ -44,26 +81,47 @@ class UserTurnState:
                     self.machine.change_state(self.machine.enemy_turn_state)
                     grabber.grabbed = None
 
-    def update(self):
+    def update(self) -> None:
+        '''Процедура кадра, вызываемая ``ChessStateMachine``'''
         self.handle_input()
 
 
 class EnemyTurnState:
-    def __init__(self, machine, brd_updater):
-        self.machine = machine
-        self.clock = pygame.time.Clock()
-        self.request_interval = game.data['request-interval']
-        self.brd_updater = brd_updater
-        self.last_check_time = 0
+    '''
+    Сущность состояния хода противника.
+    Ожидает, посылая время от времени запросы, пока противник на другом клиенте сходит, 
+    и передает ход пользователю.
+    '''
 
-    def on_start(self):
+    def __init__(self, machine: ChessStateMachine, brd_updater: board.BoardUpdater) -> None:
+        self.machine: ChessStateMachine = machine
+        '''``ChessStateMachine`` в котором вызывается данное состояние.'''
+        self.clock: pygame.time.Clock() = pygame.time.Clock()
+        '''Таймер для отсчета времени следующего запроса.'''
+        self.request_interval: int = game.data['request-interval']
+        '''Интервал запросa'''
+        self.brd_updater: board.BoardUpdater = brd_updater
+        '''Средство обновления выводимой доски.'''
+        self.last_check_time: int = 0
+        '''Время последнего запросa'''
+
+    def on_start(self) -> None:
+        '''
+        Метод, вызываемый при обновлении состояния машиной.
+        Обновляет доску.
+        '''
         brd = game.clnt.get_board()
         self.brd_updater.update_board(brd, game.clnt.is_white, False)
 
-    def update(self):
+    def update(self) -> None:
+        '''
+        Процедура кадра, вызываемая ``ChessStateMachine``.
+        Проверяет, когда был послан последний запрос, и если 
+        пришло время нового запроса, передает ход пользователю.
+        '''
         time = pygame.time.get_ticks()
-        if  self.last_check_time + self.request_interval > time:
-            return 
+        if self.last_check_time + self.request_interval > time:
+            return
 
         self.last_check_time = time
 
--- src/engine/game.py
+++ src/engine/game.py
@@ -1,20 +1,35 @@
 import json
-import threading
+from . import game_object
+import src.client as client
 import pygame
+from typing import *
 
-screen = None
-clock = None
-is_running = False
-screen_size = None
-fps = None
-cur_scene = None
-is_init = False
-data = None
-events = None
-clnt = None
+screen: pygame.surface.Surface = None
+'''Главный экран.'''
+clock: pygame.time.Clock = None
+'''Часы для отмерки fps.'''
+is_running: bool = False
+'''Состояние, показывающее, идет ли игра.'''
+screen_size: Tuple[int, int] = None
+'''Размер главного экрана.'''
+fps: int = None
+'''Число кадров в секунду в игре.'''
+cur_scene: game_object.Scene = None
+'''Текущая главная ``Scene``.'''
+is_init: bool = False
+'''Состояние, показывающее инициализирована ли ``game``.'''
+data: Dict[LiteralString, Any] = None
+'''Характеристики игры по умолчанию'''
+events: List[pygame.event.Event] = None
+'''События, произошедшие за последний кадр.'''
+clnt: client.Client = None
+'''Клиент, который будет обмениваться информацией с сервером.'''
 
 
-def init(scr_size, fps_in, data_json_path):
+def init(scr_size: Tuple[int, int], fps_in: int, data_json_path: LiteralString) -> None:
+    '''
+    Инициализатор ``game``. Все параметры соответствуют полям модуля.
+    '''
     global screen, clock, running, is_init
     global fps, cur_scene, screen_size
     global data
@@ -36,7 +51,10 @@ def init(scr_size, fps_in, data_json_path):
     with open(data_json_path) as f:
         data = json.load(f)
 
-def run():
+def run() -> None:
+    '''
+    Процедура запуска игры.
+    '''
     global running
     if not is_init:
         raise Exception('Game is not initialized')
@@ -47,7 +65,10 @@ def run():
         __iteration()
 
 
-def check_for_exit():
+def check_for_exit() -> None:
+    '''
+    Процедура, проверяющая, не было ли вызвано закрытие окна.
+    '''
     global running
 
     for event in events:
@@ -55,7 +76,10 @@ def check_for_exit():
             running = False
 
 
-def __iteration():
+def __iteration() -> None:
+    '''
+    Процедура одного кадра игры.
+    '''
     global events
     events = pygame.event.get()
 
