5cad2a9a5d1c309c25a0cb736ab45e4c614d045f
LapTapk <fret4657@gmail.com>
2023-12-18 01:54:23 +0300

    Created board docs

--- src/engine/board.py
+++ src/engine/board.py
@@ -1,16 +1,31 @@
 from . import game_object, renderer, grab, grid, game
 from .vector2 import *
-from chessLogic.Board import Board
 import pygame
+from typing import *
+import chessLogic.Board
 
 
 class BoardUpdater:
-    def init(self, go, grd):
-        self.go = go
-        self.grd = grd
+    '''Компонент, отвечающий за обновление доски в 
+    соотвествии с определенной доской из *chessLogic*'''
 
-    def create_figure(self, scene, coord,
-                      name, owned_by_user, is_user_free):
+    def init(self, go: game_object.GameObject, grd: grid.Grid):
+        '''Инициализатор. Аналогичен __init__. Все параметры соответствуют полям класса.'''
+        self.go: game_object.GameObject = go
+        '''``GameObject``, которому принадлежит компонент'''
+        self.grd: grid.Grid = grd
+        '''``Grid``, относительно которого будет строиться новая доска при обновлении'''
+
+    def create_figure(self, scene: game_object.Scene, coord: Vector2,
+                      name: LiteralString, owned_by_user: bool, is_user_free: bool) -> None:
+        '''Метод, создающий фигуру
+
+        :param scene: сцена, на которой будет располагаться фигура 
+        :param coord: относительная координата сетки, где будет располагаться фигура
+        :param name: название фигуры
+        :param owned_by_user: состояние, означающее, принадлежит ли фигура пользователю
+        :param is_user_free: состояние, означающее, сейчас ли ход пользователя
+        '''
         go = game_object.GameObject()
         rend = renderer.Renderer()
         grabable = grab.Grabable()
@@ -24,7 +39,12 @@ class BoardUpdater:
         go.init(scene, components=[rend, grabable, binder, figure_data])
         return go
 
-    def create_figures(self, board, is_white, is_user_free):
+    def create_figures(self, board: chessLogic.Board.Board, is_white: bool, is_user_free: bool):
+        '''Создает весь необходимый набор фигур 
+
+        :param board: логическая доска, по которой строится доска для пользователя
+        :param is_white: состояние, показывающее, играет ли за белых пользватель
+        :param is_user_free: состояние, показывающее, ход ли сейчас пользователя'''
         user_color = 'white' if is_white else 'black'
         res = []
         for i in range(8):
@@ -44,18 +64,29 @@ class BoardUpdater:
                 res.append(figure_go)
         return res
 
-    def update_board(self, board, user_color, is_user_free):
-        figures = self.create_figures(board, user_color, is_user_free)
+    def update_board(self, board, is_white, is_user_free):
+        '''Процедура, обновляющая доску
+
+        :param board: логическая доска, по которой будет строиться пользовательская доска
+        :param is_white: состояние, показывающее, играет ли за белых пользватель
+        :param is_user_free: состояние, показывающее, ход ли сейчас пользователя'''
+        figures = self.create_figures(board, is_white, is_user_free)
         self.go.children = figures
 
     def update(self):
+        '''Пустой метод кадра компонента'''
         pass
 
 
 class FigureData:
+    '''Вспомогательный компонент, содержащий некоторые характеристики фигуры.'''
     def init(self, go, binder):
+        '''Инициализатор. Аналогичен __init__. Все параметры соответствуют полям класса'''
         self.go = go
+        '''``GameObject``, которму принадлежит компонент'''
         self.binder = binder
+        '''``Binder``, закрепляющий фигуру за клеткой'''
 
     def update(self):
+        '''Пустой метод кадра компонента'''
         pass
