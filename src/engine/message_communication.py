3330834f15dea97c0d314e5564fa8c44525880f6
LapTapk <fret4657@gmail.com>
2023-12-18 01:27:09 +0300

    Created message_communication docs

--- src/engine/message_communication.py
+++ src/engine/message_communication.py
@@ -1,24 +1,45 @@
 import pygame
-from . import game
+from typing import *
+from . import game, game_object, chess_state_machine, renderer
 
 
 class MessageCommunication:
-    def init(self, go, font_size: int, chess_machine, rend) -> None:
-        self.go = go
-        self.last_check_time = 0
-        self.msg_surface = None
-        self.chess_machine = chess_machine
-        self.cur_msg = None
-        self.font_size = font_size
-        self.rend = rend
+    '''Компонент, отвечающий за восприятие сигналов отправки сообщений противнику 
+    и вывод их на экран'''
+    def init(self, go: game_object.GameObject, font_size: int, chess_machine: chess_state_machine.ChessStateMachine, rend: renderer.Renderer) -> None:
+        '''Инициализатор. Аналогичен __init__. Все параметры соответствуют полям класса'''
+        self.go: game_object.GameObject = go
+        '''``GameObject``, к которому прикреплен компонент'''
+        self.last_check_time: int = 0
+        '''Время последней проверки наличия сообщения'''
+        self.msg_surface: pygame.surface.Surface = None
+        '''``Surface`` текста последнего сообщения'''
+        self.chess_machine: chess_state_machine.ChessStateMachine = chess_machine
+        '''``ChessStateMachine``, необходимый для переключения на конец игры'''
+        self.cur_msg: Dict[LiteralString, LiteralString | bool] = None
+        '''Последнее сообщение'''
+        self.font_size: int = font_size
+        '''Размер шрифта выводимого сообщния'''
+        self.rend: renderer.Renderer = rend
+        '''``Renderer``, с помощью которого будет выводится текст сообщения'''
 
-    def show_msg(self, msg) -> None:
+    def show_msg(self, msg: Dict[LiteralString, LiteralString | bool]) -> None:
+        '''Процедура, выводящая сообщение на экран
+        
+        :param msg: сообщение, которое необходимо вывести'''
         base_font = pygame.font.Font(None, self.font_size)
         msg_surface = base_font.render(
             msg, False, pygame.color.THECOLORS['black'])
         self.rend.img = msg_surface
 
-    def handle_input(self, event) -> None:
+    def handle_input(self, event: pygame.event.Event) -> None:
+        '''
+        Метод, обрабатывающий ввод игрока. 
+        Стрелка вверх - сообщение о признании себя побежденным / да.
+        Стрелка вниз - сообщение с предложением ничьи / нет
+
+        :param event: событие, которое необходимо обработать.
+        '''
         if event.type != pygame.KEYDOWN:
             return
 
@@ -41,7 +62,9 @@ class MessageCommunication:
 
         self.show_msg(msg)
 
-    def check_msg(self):
+    def check_msg(self) -> None:
+        '''Метод, выполняющий проверку на наличие сообщения к себе
+        через некий интервал времени.'''
         time = pygame.time.get_ticks()
         interval = game.data['request-interval']
         if self.last_check_time + interval >= time:
@@ -57,11 +80,16 @@ class MessageCommunication:
         self.show_msg(msg['text'])
         self.handle_response(msg)
 
-    def handle_response(self, msg):
+    def handle_response(self, msg: Dict[LiteralString, LiteralString | bool]) -> None:
+        '''Метод, обрабатывающий входящее сообщение.
+        Если пришло сообщение с соглашением на ничью или с признанием поражения
+        
+        :param msg: сообщение, которое нужно обработать'''
         if msg['text'] == 'Ок' or msg['text'] == 'Сдаюсь':
             self.chess_machine.change_state(self.chess_machine.end_state)
 
-    def update(self):
+    def update(self) -> None:
+        '''Метод кадра компонента'''
         for event in game.events:
             self.handle_input(event)
         self.check_msg()
