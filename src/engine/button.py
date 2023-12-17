1f4ea3bc5929e174c8dfca5940cb4ba85751fc94
LapTapk <fret4657@gmail.com>
2023-12-17 23:05:58 +0300

    Wrote button docs

--- src/engine/button.py
+++ src/engine/button.py
@@ -1,14 +1,22 @@
 import pygame
-from . import game
+from . import game, game_object, renderer
+from typing import *
 
 
 class Button:
-    def init(self, go, rend, *funcs):
-        self.go = go
-        self.rend = rend
-        self.funcs = funcs
+    '''Компонент кнопки, выполняющей определенные действия при нажатии'''
+    def init(self, go: game_object.GameObject, rend: renderer.Renderer, *funcs: Callable[[], None]):
+        self.go: game_object.GameObject = go
+        '''``GameObject`` к которому прикреплен компонент'''
+        self.rend: renderer.Renderer = rend
+        '''``Renderer`` с помощью которого кнопка выведется на экран'''
+        self.funcs: Callable[[], None] = funcs
+        '''События, которын должны произойти при нажатии кнопки'''
 
-    def is_clicked(self):
+    def is_clicked(self) -> bool:
+        '''Метод, проверяющий была ли нажата кнопка в данный кадр.
+        
+        :return: была ли кнопка нажата'''
         rect = self.rend.get_rect()
 
         m_pos = pygame.mouse.get_pos()
@@ -18,7 +26,8 @@ class Button:
 
         return over_button and mouse_down
 
-    def update(self):
+    def update(self) -> None:
+        '''Метод кадра компонента'''
         if not self.is_clicked():
             return
 
