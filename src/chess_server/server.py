8e7cb233246f9a9a47fe564795dfde77d83e3cd2
LapTapk <fret4657@gmail.com>
2023-12-17 22:28:36 +0300

    Made server docs

--- src/chess_server/server.py
+++ src/chess_server/server.py
@@ -1,14 +1,25 @@
+'''
+Модуль, ответственный за единственный http сервер в приложении.
+'''
+from typing import *
 import http.server
 import threading
 from chessLogic.Board import Board
 import re
 import json
 
+
 server = None
+'''
+Экземпляр сервера. Пока сервер не инициализирован равняет None
+'''
 is_init = False
+'''Состояние, показывающее инициализирован ли сервер'''
 
 
-def __start_server():
+def __start_server() -> None:
+    '''Закрытая функция, запускающая сервер и, в случае закрытия 
+    приложения, закрывающая его.'''
     try:
         server.serve_forever()
     except:
@@ -16,7 +27,12 @@ def __start_server():
         exit()
 
 
-def init(address):
+def init(address: Tuple[LiteralString, int]) -> None:
+    '''Инициализатор сервера. Запускает функцию *__start_server* 
+    парралельно основной программе.
+
+    :param address: адрес, который будет иметь запускаемый сервер.
+    '''
     global server, is_init
     if is_init:
         raise Exception('Server s already initialized')
@@ -32,21 +48,40 @@ def init(address):
 
 
 class Server(http.server.HTTPServer):
-    def __init__(self, server_address, handler, brd):
+    '''Сущность сервера http.'''
+
+    def __init__(self, server_address: Tuple[LiteralString, int], handler: http.server.BaseHTTPRequestHandler, brd: Board):
+        '''
+        :param server_address: адрес сервера.
+        :param handler: тип обработчика запросов.
+        :param brd: шаматная доска, хранимая на сервере.
+        '''
         super().__init__(server_address, handler)
-        self.brd = brd
-        self.moves_cnt = 0
-        self.connected = 0
-        self.msg_black = None
-        self.msg_white = None
-        self.state = 'neutral'
+        self.brd: Board = brd
+        '''Шахматная доска, хранимая на сервере.'''
+        self.moves_cnt: int = 0
+        '''Количество проведенных ходов.'''
+        self.connected: int = 0
+        '''Количество подключенных игроков.'''
+        self.msg_black: Dict[LiteralString, LiteralString | bool] | None = None
+        '''Сообщение, переданне черным.'''
+        self.msg_white: Dict[LiteralString, LiteralString | bool] | None = None
+        '''Сообщение, переданное белым.'''
+        self.state: LiteralString | Tuple[LiteralString,
+                                          LiteralString] = 'neutral'
+        '''Поле, хранящее состояние, в каком сейчас находится игра.
+        Может быть neutral, (checkmate, color), (palemate, color), 
+        где color - цвет игрока, на ходе которого случилось событие'''
 
 
 class ReqauestHandler(http.server.BaseHTTPRequestHandler):
-    def check_for_new_con(self):
-        self.server.connected.add(self.address_string)
-
-    def get_msg(self, for_white):
+    '''Класс обработчика запросов для сервера.'''
+    def get_msg(self, for_white: bool) -> None:
+        '''Метод, отвечающий за отправку сообщения для белых или черных
+        клиенту.
+        
+        :param for_white: состояние, указывающее на то, должно ли быть передано сообщение для белых или для черных
+        '''
         msg = self.server.msg_white if for_white else self.server.msg_black
         data = None
         if msg == None:
@@ -55,7 +90,14 @@ class ReqauestHandler(http.server.BaseHTTPRequestHandler):
             data = json.dumps(msg).encode()
         self.wfile.write(data)
 
-    def do_GET(self):
+    def do_GET(self) -> None:
+        '''Метод обработки запроса GET. 
+        path == /moves_cnt -> отправка move_cnt.
+        path == /board -> отправка brd.
+        path == /is_chess -> отправляет утверждение, что это сервер для шахмат.
+        path == /conn -> отправка connected.
+        path == /msg_black|/msg_white -> отправка сообщения для черных|белых.
+        path == /state -> отправка состояния на текущий момент'''
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
@@ -75,12 +117,19 @@ class ReqauestHandler(http.server.BaseHTTPRequestHandler):
         elif self.path == '/state':
             self.wfile.write(str(self.server.state).encode())
 
-    def post_conn(self):
+    def post_conn(self) -> None:
+        '''Метод, отвечающий за прием нового подключившегося клиента.'''
         self.server.connected += 1
         self.send_response(200)
         self.end_headers()
 
-    def post_move(self):
+    def post_move(self) -> None:
+        '''Метод, отвечающий за прием нового хода. 
+        если ход легальный, возвращает код 200, 
+        если нелегальный, возвращает 403, 
+        если ход передан в некправильнои формате, возвращает 400.
+        Если код 200, значит был сделан ход и обновлено число ходов.
+        '''
         length = int(self.headers['Content-Length'])
         move = self.rfile.read(length).decode()
 
@@ -104,7 +153,10 @@ class ReqauestHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-    def post_msg(self, sender_is_white):
+    def post_msg(self, sender_is_white: bool) -> None:
+        '''Метод, отвечающий за прием сообещния для белых|черных
+        
+        :param sender_is_white: состояние, показывающее от кого было прислано сообщение'''
         msg_sender = self.server.msg_white if sender_is_white else self.server.msg_black
         length = int(self.headers['Content-Length'])
         msg = self.rfile.read(length)
@@ -129,6 +181,11 @@ class ReqauestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
+        '''Метод, отвечающий за прием запросов.
+        path == /conn -> *post_conn*.
+        path == /move -> *post_move*.
+        path == /msg/black -> *post_msg(True)*.
+        path == /msg/white -> *post_msg(False)*'''
         if self.path == '/conn':
             self.post_conn()
         elif self.path == '/move':
