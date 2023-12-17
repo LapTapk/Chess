320fd2ff0735878acd5c3bb5e62437b852faf7a5
LapTapk <fret4657@gmail.com>
2023-12-17 22:53:49 +0300

    Created docs for client

--- src/chess_server/client.py
+++ src/chess_server/client.py
@@ -1,20 +1,36 @@
+from typing import *
 import http.client
 import json
 from chessLogic.Board import Board
-from engine import message_communication
-
 
 class Client:
-    def __init__(self, host, port, is_white):
-        self.address = (host, port)
-        self.is_white = is_white
-        self.local_moves_cnt = 0
+    '''Класс клиента, отвественный за запросы к серверу.'''
+    def __init__(self, host: LiteralString, port: int, is_white: bool):
+        '''
+        При создании класса отправляет серверу запрос POST, 
+        гласящем о подключении клиента. 
+
+        :param host: адрес сервера, к которому будет подключаться клиент 
+        :param port: порт сервера, к которому будет подключаться клиент
+        :param is_white: покзывает, играет ли этот клиент за белых.
+        '''
+        self.address: Tuple[LiteralString, int] = (host, port)
+        '''Адрес сервера, к которому будет подключаться клиент'''
+        self.is_white: bool = is_white
+        '''Состояние, показывающее играет ли этот клиент за белых или за черных'''
+        self.local_moves_cnt: int = 0
+        '''Число ходов, которые зафиксировал клиент. Необходимо для обновления доски.'''
 
         conn = http.client.HTTPConnection(host, port)
         conn.request('POST', '/conn')
         conn.close()
 
-    def send_move(self, frm, to):
+    def send_move(self, frm: Tuple[int, int], to: Tuple[int, int]) -> bool:
+        '''Метод, отвечающий за отправку хода северу 
+        
+        :param frm: откуда перенесена фигура.
+        :param to: куда перенесена фигура.
+        :return: был ли сделан ход'''
         if self.is_white:
             frm = (7 - frm[0], 7 - frm[1])
             to = (7 - to[0], 7 - to[1])
@@ -28,7 +44,10 @@ class Client:
         response = conn.getresponse()
         return response.getcode() == 200
 
-    def get_board(self):
+    def get_board(self) -> Board:
+        '''Метод, отвечающий за получение актуального состояния доски с сервера.
+        
+        :return: версия поля с сервера.'''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         conn.request('GET', '/board')
@@ -40,25 +59,38 @@ class Client:
 
         return Board.deserialize(b_data)
 
-    def get_moves(self):
+    def get_moves(self) -> int:
+        '''Метод, отвечающий за получение кол-ва поизведенных ходов с сервера.
+
+        :return: актуальное кол-во произведенных ходов.
+        '''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         conn.request('GET', '/moves_cnt')
         response = conn.getresponse()
         return int(response.read().decode())
 
-    def has_moved(self):
+    def has_moved(self) -> bool:
+        '''Метод, проверяющий был ли произведен ход
+        
+        :return: был ли произведен ход'''
         server_moves_cnt = self.get_moves()
         return self.local_moves_cnt != server_moves_cnt
 
-    def get_conn(self):
+    def get_conn(self) -> int:
+        '''Метод, возвращающий кол-во подключений к серверу.
+        
+        :return: кол-во подключений к серверу.'''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         conn.request('GET', '/conn')
         response = conn.getresponse()
         return int(response.read().decode())
 
-    def get_msg(self):
+    def get_msg(self) -> Dict[LiteralString, LiteralString | bool]:
+        '''Метод, получаеющий сообщение для цвета данного клиента.
+        
+        :return: сообщение для цвета данного клиента.'''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         color = 'white' if self.is_white else 'black'
@@ -72,7 +104,12 @@ class Client:
         data = json.loads(data)
         return data
 
-    def send_msg(self, text, is_response):
+    def send_msg(self, text: LiteralString, is_response: bool) -> bool:
+        '''Метод, отправляющий сообщение противоположному цвету.
+        
+        :param text: текст сообщения
+        :param is_response: обозначает, является ли отправляемое сообщение ответом на вопрос
+        :return: было ли принято сообщение'''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         color = 'black' if self.is_white else 'white'
@@ -84,7 +121,10 @@ class Client:
         response = conn.getresponse()
         return response.getcode == 200 
 
-    def get_state(self):
+    def get_state(self) -> LiteralString | Tuple[LiteralString, LiteralString]:
+        '''Метод, получающий состояние на текущий ход с сервера.
+
+        :return: статус игры на текущий ход'''
         host, port = self.address
         conn = http.client.HTTPConnection(host, port)
         conn.request('GET', '/state')
