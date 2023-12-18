6fb4b2a5a7b250e8afbdf15eed19c561ffa62c93
LapTapk <fret4657@gmail.com>
2023-12-18 17:07:20 +0300

    Added connection_checker docs

--- src/engine/connection_checker.py
+++ src/engine/connection_checker.py
@@ -1,6 +1,9 @@
 from . import game, scenes
 
 class ConnectionChecker:
-    def update(self):
+    '''Компонент, проверяющий, подключилось ли достаточное число игроков,
+    и переключающий в таком случае сцену на игровую'''
+    def update(self) -> None:
+        '''Метод кадра компоента'''
         if game.clnt != None and game.clnt.get_conn() == 2:
             game.cur_scene = scenes.create_chess_scene()
