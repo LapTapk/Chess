c37c7fb63e4c4d1d2cd7922c30af537f43b59f37
LapTapk <fret4657@gmail.com>
2023-12-18 19:31:28 +0300

    Updated starts

--- start_win.bat
+++ start_win.bat
@@ -1,2 +1,11 @@
 cd %~d0
-python src/main.py
+if exist .venv\Scripts\activate.bat (
+    echo
+) else (
+ python -m virtualenv .venv
+ CALL .venv\Scripts\activate.bat
+ pip install pygame
+ pip install setuptools
+ python setup.py install
+)
+ python src/main.py
